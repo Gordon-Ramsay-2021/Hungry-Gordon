949b536bc696329bce16ea5d65710518fe354f2e
zak <19314863+zakarybk@users.noreply.github.com>
2021-01-31 12:35:19 +0000

    Pause menu attempt

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUI;
+
+    private void Awake()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        GameIsPaused = true;
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        GameIsPaused = false;
+
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OpenMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
